9c2ec0b2c5eb9909247677e98af8e8ff32fcd088
Gabor Kalmar <gakalmar@gmail.com>
2024-07-14 09:00:25 +0200

    Copy Client from previous project

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const OtherPage = () => {
+    return (
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <Link to="/">Back</Link>
+      </div>
+    );
+  };
+  
+  export default OtherPage;
